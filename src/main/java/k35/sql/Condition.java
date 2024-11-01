2b79de957778254efca20f3e12d991e67406a40e
Pavel Kopychenko <t34box@gmail.com>
2024-11-01 16:56:47 +0300

    Conditions...

--- src/main/java/k35/sql/Condition.java
+++ src/main/java/k35/sql/Condition.java
@@ -1,53 +1,229 @@
 package k35.sql;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
+import java.util.function.UnaryOperator;
 
 public final class Condition implements SqlBuilder {
 
-    public enum Op {
-        AND,
-        OR,
-        EXPR
-    }
+	public static class Value {
+		private final String value1;
 
-    public final Op op;
-    public final List<Condition> childs;
-    public final Optional<String> expr;
+		private Value(String value1) {
+			this.value1 = value1;
+		}
 
-    private Condition(Op op, List<Condition> childs, Optional<String> expr) {
-        this.op = op;
-        this.childs = childs;
-        this.expr = expr;
-    }
+		public static Value of(String value1) {
+			return new Value(value1);
+		}
 
-    public static Condition and(Condition... childs) {
-        return new Condition(Op.AND, Arrays.asList(childs), Optional.empty());
-    }
+		/**
+		 * = (some bind value)
+		 * 
+		 * @param value2
+		 * @return
+		 */
+		public String eq(String value2) {
+			return equal(value2);
+		}
 
-    public static Condition or(Condition... childs) {
-        return new Condition(Op.OR, Arrays.asList(childs), Optional.empty());
-    }
+		/**
+		 * = (some bind value)
+		 * 
+		 * @param value2
+		 * @return
+		 */
+		public String equal(String value2) {
+			return this.value1 + " = " + value2;
+		}
 
-    public static Condition expression(String expr) {
-        return new Condition(Op.EXPR, new ArrayList<>(), Optional.ofNullable(expr));
-    }
+		/**
+		 * <> (some bind value)
+		 * 
+		 * @param value2
+		 * @return
+		 */
+		public String ne(String value2) {
+			return notEqual(value2);
+		}
 
-    public String buildSql() {
+		/**
+		 * <> (some bind value)
+		 * 
+		 * @param value2
+		 * @return
+		 */
+		public String notEqual(String value2) {
+			return this.value1 + " <> " + value2;
+		}
 
-        if (op == Op.EXPR)
-            return expr.orElse("");
+		/**
+		 * < (some bind value)
+		 * 
+		 * @param value2
+		 * @return
+		 */
+		public String lt(String value2) {
+			return lessThen(value2);
+		}
 
-        final String joinOpStr = this.op == Op.OR ? "or" : "and";
-        return "( "
-                + this.childs
-                        .stream()
-                        .map(Condition::buildSql)
-                        .collect(Collectors.joining(" " + joinOpStr + " "))
-                + " )";
-    }
+		/**
+		 * < (some bind value)
+		 * 
+		 * @param value2
+		 * @return
+		 */
+		public String lessThen(String value2) {
+			return this.value1 + " < " + value2;
+		}
 
+		/**
+		 * <= (some bind value)
+		 * 
+		 * @param value2
+		 * @return
+		 */
+		public String le(String value2) {
+			return lessOrEqual(value2);
+		}
+
+		/**
+		 * <= (some bind value)
+		 * 
+		 * @param value2
+		 * @return
+		 */
+		public String lessOrEqual(String value2) {
+			return this.value1 + " <= " + value2;
+		}
+
+		/**
+		 * > (some bind value)
+		 * 
+		 * @param value2
+		 * @return
+		 */
+		public String gt(String value2) {
+			return greaterThan(value2);
+		}
+
+		/**
+		 * > (some bind value)
+		 * 
+		 * @param value2
+		 * @return
+		 */
+		public String greaterThan(String value2) {
+			return this.value1 + " > " + value2;
+		}
+
+		/**
+		 * >= (some bind value)
+		 * 
+		 * @param value2
+		 * @return
+		 */
+		public String ge(String value2) {
+			return greaterOrEqual(value2);
+		}
+
+		/**
+		 * >= (some bind value)
+		 * 
+		 * @param value2
+		 * @return
+		 */
+		public String greaterOrEqual(String value2) {
+			return this.value1 + " >= " + value2;
+		}
+
+		public String between(String from, String to) {
+			return "between " + from + " and " + to;
+		}
+
+		public String notBetween(String from, String to) {
+			return "not between " + from + " and " + to;
+		}
+
+		public String notEq(String value2) {
+			return value1 + " <> " + value2;
+		}
+
+		public String is(String value2) {
+			return value1 + " is " + value2;
+		}
+
+		public String isNot(String value2) {
+			return value1 + " is not " + value2;
+		}
+
+		public String isTrue() {
+			return value1 + " is true";
+		}
+
+		public String isNotTrue() {
+			return value1 + " is not true";
+		}
+
+		public String isFalse() {
+			return value1 + " is false";
+		}
+
+		public String isNotFalse() {
+			return value1 + " is not false";
+		}
+
+		public String isNull() {
+			return value1 + " is null";
+		}
+
+		public String isNotNull() {
+			return value1 + " is not null";
+		}
+
+		public String in(String value2) {
+			return value1 + " in ( " + value2 + " )";
+		}
+
+		public String isUnknown() {
+			return value1 + " is unknown";
+		}
+
+		public String isNotUnknown() {
+			return value1 + " is not unknown";
+		}
+	}
+
+	private final String expression;
+
+	private Condition(String expression) {
+		this.expression = expression;
+	}
+
+	public static Condition expression(String expression) {
+		return new Condition(expression);
+	}
+
+	public Condition and(Condition condition) {
+		return new Condition(this.expression + " and (" + condition.buildSql() + ")");
+	}
+
+	public Condition and(String expression) {
+		return new Condition(this.expression + " and " + expression);
+	}
+
+	public Condition or(Condition condition) {
+		return new Condition(this.expression + " or (" + condition.buildSql() + ")");
+	}
+
+	public Condition or(String expression) {
+		return new Condition(this.expression + " or " + expression);
+	}
+
+	public Condition map(UnaryOperator<Condition> fn) {
+		return fn.apply(this);
+	}
+
+	@Override
+	public String buildSql() {
+		return this.expression;
+	}
 }
--- src/test/java/k35/sql/ConditionTest.java
+++ src/test/java/k35/sql/ConditionTest.java
@@ -2,37 +2,51 @@ package k35.sql;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.function.UnaryOperator;
+
 import org.junit.Test;
 
 public class ConditionTest {
 
-    @Test
-    public void testExpression() {
-        assertEquals("status = 'success'",
-                Condition.expression("status = 'success'").buildSql());
-    }
+	@Test
+	public void testExpression() {
+		assertEquals("status = 'success'", Condition.expression("status = 'success'").buildSql());
+	}
 
-    @Test
-    public void testAnd() {
-        assertEquals("( a < 1 and b > 100 )", Condition.and(
-                Condition.expression("a < 1"),
-                Condition.expression("b > 100")).buildSql());
-    }
+	@Test
+	public void testAnd() {
+		assertEquals("a < 1 and b > 100", Condition.expression("a < 1").and("b > 100").buildSql());
 
-    @Test
-    public void testOr() {
-        assertEquals("( a < 1 or b > 100 )", Condition.or(
-                Condition.expression("a < 1"),
-                Condition.expression("b > 100")).buildSql());
-    }
+		final var c1 = Condition.expression("c = 1").and("d = 2");
 
-    @Test
-    public void testAll() {
-        assertEquals("( a < 1 or ( b > 100 and status = 'success' ) )", Condition.or(
-                Condition.expression("a < 1"),
-                Condition.and(
-                        Condition.expression("b > 100"),
-                        Condition.expression("status = 'success'")))
-                .buildSql());
-    }
+		assertEquals("a < 1 and b > 100 and (c = 1 and d = 2)",
+				Condition.expression("a < 1").and("b > 100").and(c1).buildSql());
+	}
+
+	@Test
+	public void testOr() {
+		assertEquals("a < 1 or b > 100", Condition.expression("a < 1").or("b > 100").buildSql());
+
+		final var c1 = Condition.expression("c = 1").and("d = 2");
+
+		assertEquals("a < 1 or b > 100 or (c = 1 and d = 2)",
+				Condition.expression("a < 1").or("b > 100").or(c1).buildSql());
+	}
+
+	@Test
+	public void testMap() {
+
+		final var c1 = Condition.expression("c = 1").and("d = 2");
+
+		final UnaryOperator<Condition> fn = c -> c.and(c1);
+
+		assertEquals("a < 1 or b > 100 and (c = 1 and d = 2)",
+				Condition.expression("a < 1").or("b > 100").map(fn).buildSql());
+	}
+
+	@Test
+	public void testAll() {
+		assertEquals("a < 1 or b > 100 and status = 'success'",
+				Condition.expression("a < 1").or("b > 100").and("status = 'success'").buildSql());
+	}
 }
